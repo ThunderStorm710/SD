5486b33349896fee7830ba50cc004b5325c1b621
Pedro Ascensão <uc2020233012@student.uc.pt>
2023-03-16 17:34:21 +0000

    Início

---
+++ FilaURL.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FilaURL {
+    HashMap<String, HashSet<String>> index;
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
